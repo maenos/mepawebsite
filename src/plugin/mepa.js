0e6d89cb38a3458ebe6f3d8392d29281f076e7b6
maenos <66688267+maenos@users.noreply.github.com>
2025-06-15 06:01:54 +0200

    push

--- src/plugin/mepa.js
+++ src/plugin/mepa.js
@@ -2,11 +2,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  //  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://api.eglisesiloecentre.org/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    'X-API-KEY': '48e81e4d4f4164ecce03a139bc0fd407ccf50ab77a1aa5fe250ef067600e99d2', // si tu veux la gérer via .env
+    'X-API-KEY': 'Sc_7Qm9pL3vN8wR2tK5hJ6bX4zF1gY0aS', // si tu veux la gérer via .env
   },
   timeout: 10000,
 })
